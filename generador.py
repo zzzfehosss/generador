a8cc851e5f12eacdb8cedb1293f60dd8c78c2649
zzzfehosss <104534151+zzzfehosss@users.noreply.github.com>
2022-04-27 18:09:22 -0400

    Add files via upload

---
+++ generador.py
@@ -0,0 +1,14 @@
+import random
+
+minus = "abcdefghijklmnñopqrstuvwxyz"
+mayus = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+numeros = "0123456789"
+
+simbolos = "@$&#/)!?'*'"
+
+nose = minus + mayus + numeros + simbolos
+
+nosex2 = 10
+contra = "".join(random.sample(nose, nosex2))
+
+print("tu contraseña es:", contra)
